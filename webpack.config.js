b975e720fe3e594b6af96552fd0d5e528b81291c
sho <sho_ws@outlook.com>
2021-06-06 12:39:01 +0900

    fs解決

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,14 @@
 const path = require('path');
+const fs = require('fs');
+
+const nodeModules = {};
+fs.readdirSync('node_modules')
+  .filter(function(x) {
+    return ['.bin'].indexOf(x) === -1;
+  })
+  .forEach(function(mod) {
+    nodeModules[mod] = 'commonjs ' + mod;
+  });
 
 module.exports = {
 // モード値を production に設定すると最適化された状態で、
@@ -12,6 +22,7 @@ output: {
     path: path.join(__dirname, "./dist"),
     filename: "index.js"
 },
+externals: nodeModules,
 
 module: {
     rules: [{
